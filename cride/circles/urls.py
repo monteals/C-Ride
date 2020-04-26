32cc882ef755a083524e0e93c747f0e4c42ceb2c
Sebastian Montealegre <smontealegrebrand@gmail.com>
2020-04-26 16:38:46 -0500

    Creacion de un API con Rest Framework

--- cride/circles/urls.py
+++ cride/circles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from cride.circles.views import list_circles
+from cride.circles.views import list_circles, create_cricle
 
 urlpatterns = [
     path('circles/', list_circles),
+    path('circles/create/', create_cricle),
 ]
--- cride/circles/views.py
+++ cride/circles/views.py
@@ -1,9 +1,10 @@
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from cride.circles.models import Circle
 
+@api_view(['GET'])
 def list_circles(request):
-    circles = Circle.objects.all()
-    public = circles.filter(is_public=True)
+    circles = Circle.objects.filter(is_public=True)
     data = []
     for circle in circles:
         data.append({
@@ -13,5 +14,20 @@ def list_circles(request):
             'rides_offered': circle.rides_offered,
             'members_limit': circle.members_limit,
         })
-    return JsonResponse(data, safe=False)
+    return Response(data)
+
+@api_view(['POST'])
+def create_cricle(request):
+    name = request.data['name']
+    slug_name = request.data['slug_name']
+    about = request.data.get('about', '')
+    circle = Circle.objects.create(name=name, slug_name=slug_name, about=about)
+    data = {
+        'name': circle.name,
+        'slug_name': circle.slug_name,
+        'rides_taken': circle.rides_taken,
+        'rides_offered': circle.rides_offered,
+        'members_limit': circle.members_limit,
+    }
+    return Response(data)
 
