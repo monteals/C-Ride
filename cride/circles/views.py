232f9a86cd92c26a6334b0db258238e09363c425
Sebastian Montealegre <smontealegrebrand@gmail.com>
2020-04-26 15:56:52 -0500

    API sin Django Rest Framework e instalación del requerimiento

--- cride/circles/views.py
+++ cride/circles/views.py
@@ -1,5 +1,17 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
+from cride.circles.models import Circle
 
 def list_circles(request):
-    return HttpResponse('hola')
-    
+    circles = Circle.objects.all()
+    public = circles.filter(is_public=True)
+    data = []
+    for circle in circles:
+        data.append({
+            'name': circle.name,
+            'slug_name': circle.slug_name,
+            'rides_taken': circle.rides_taken,
+            'rides_offered': circle.rides_offered,
+            'members_limit': circle.members_limit,
+        })
+    return JsonResponse(data, safe=False)
+
